460f8042300352f43e81c83949b3e8a61e334a44
Franco Flores <francobarbosaflores@gmail.com>
2022-09-18 17:56:47 -0300

    :tada: using ASCII table

---
+++ aulas/testAula01/charA.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char charA = 65;
+    char charF = 70;
+    char charR = 82;
+    char charN = 78;
+    char charC = 67;
+    char charO = 79;
+
+    printf(" %c \n", charA);
+
+    printf("Meu nome: ");
+    printf("%C%C%C%C%C%C\n\n",charF, charR, charA, charN, charC,charO );
+    return 0;  
+
+}
