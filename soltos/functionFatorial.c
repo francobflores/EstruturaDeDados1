8701721f283c4f77f106ab2f2352c0cbd7f15890
Franco Flores <francobarbosaflores@gmail.com>
2022-10-05 15:32:28 -0300

    :pencil: arranjo

--- soltos/functionFatorial.c
+++ soltos/functionFatorial.c
@@ -14,12 +14,50 @@ void fat( int n )
     printf("Fatorial = %d\n", f);
 
 }
+/* Função que calcula o fatorial e retorna o seu valor */
+int fatWithReturn( int n )
+{
+    int f = 1;
+    int i ;
+
+    for(i = 1; i <= n; i++)
+        f*=i;
+    
+    return f;
+
+
+}
+
+/* Função Arranjos */
+
+int arranjo( int n, int k )
+{
+    int a = fatWithReturn(n) / fatWithReturn(n-k);
+    return a;
+}
 
 /* Função Principal */
 
 int main(void)
 {
     int n;
+    int aj;
+    int k;
+/*
+    printf("Informe valor do numero inteiro não negativo que deseja obter o fatorial:");
     scanf("%d", &n);
-    fat(n);
+    int fat = fatWithReturn(n);
+    printf("Fatorial de %d é %d\n",n, fat);
+*/
+    printf("informe n:");
+    scanf("%d", &n);
+    printf("informe k:");
+    scanf("%d", &k);
+
+    aj = arranjo(n, k);
+
+    printf("Arranjo é %d\n",aj);
+
+    
+
 }
